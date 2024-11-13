8206daa08296148fa3f2a4b242a5c5a9ce8ed67a
tekahvec <tekahvec@ncsu.edu>
2024-11-13 10:41:38 -0500

    searchbar improvements with added frontend for filter and sort

--- website/components/PlantSearch/Header.tsx
+++ website/components/PlantSearch/Header.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Heading } from "../heading";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>
-      <Heading>Plant Database</Heading>
+    <div className="relative ">
+      <Image
+        src="/plant-search.png"
+        alt="Plant Database"
+        width={800}
+        height={200}
+        className="w-full"
+        priority
+      />
+
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
+        <div className="bg-gray-400 bg-opacity-90 p-4 md:p-6 rounded-lg">
+          <Heading className="text-4xl md:text-5xl font-bold mb-2 text-green-700">
+            Discover
+          </Heading>
+          <Heading className="text-lg md:text-xl sm:text-xl text-black">
+            Amazing Plants in North Carolina
+          </Heading>
+        </div>
+      </div>
     </div>
   );
 };

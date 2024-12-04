fa177b47e20314d9aa7c8e1c2665a67f24b3b194
Armigerous <tugraerenk23@gmail.com>
2024-12-04 06:15:15 -0500

    fix: Wrap Contact page content in MaxWidthWrapper for improved layout and responsiveness

--- website/app/(about)/(noroll)/contact/page.tsx
+++ website/app/(about)/(noroll)/contact/page.tsx
@@ -5,6 +5,7 @@ import LottieAnimation from "@/components/Contact/LottieAnimation";
 import ContactForm from "@/components/Contact/ContactForm";
 // Import site metadata utility for accessing global site information
 import siteMetaData from "@/lib/siteMetaData";
+import { MaxWidthWrapper } from "@/components/maxWidthWrapper";
 
 // Define and export metadata for the Contact page
 export const metadata: Metadata = {
@@ -15,43 +16,40 @@ export const metadata: Metadata = {
 // Define the Contact functional component using React
 const Contact: React.FC = () => {
   return (
-    <section
-      className="w-full border-b-2 border-solid flex items-center justify-center 
-      flex-col md:flex-row
-      h-auto md:h-[75vh]
-      text-dark dark:text-light
-      border-dark dark:border-light"
-      // Styling the main container to be responsive and adaptable to light/dark modes
-    >
-      <div
-        className="inline-block h-full border-solid 
-        w-full md:w-2/5
-        border-dark dark:border-light
-        border-b-2 md:border-b-0 md:border-r-2"
-        // Styling the animation container to be responsive and adaptable to light/dark modes
+    <MaxWidthWrapper>
+      <section
+        className="w-full flex items-center justify-center 
+      flex-col md:flex-row h-auto md:h-[75vh] text-cream-800"
+        // Styling the main container to be responsive and adaptable to light/dark modes
       >
-        {/* Render the LottieAnimation component */}
-        <LottieAnimation />
-      </div>
-      <div
-        className="flex flex-col justify-center pb-8
-        py-6 md:py-0
+        <div
+          className="inline-block h-full  
+        w-full md:w-2/5"
+          // Styling the animation container to be responsive and adaptable to light/dark modes
+        >
+          {/* Render the LottieAnimation component */}
+          <LottieAnimation />
+        </div>
+        <div
+          className="flex flex-col justify-center
+        py-8 md:py-0
         w-full md:w-3/5
         px-5 xs:px-10 md:px-16
         items-center md:items-start"
-        // Styling the form container to be responsive and adaptable to light/dark modes
-      >
-        <h2
-          className="font-bold capitalize 
-          text-2xl xs:text-3xl sm:text-4xl"
-          // Styling the heading to be responsive
+          // Styling the form container to be responsive and adaptable to light/dark modes
         >
-          Contact Us!
-        </h2>
-        {/* Render the ContactForm component */}
-        <ContactForm />
-      </div>
-    </section>
+          <h2
+            className="font-bold capitalize 
+          text-2xl xs:text-3xl sm:text-4xl"
+            // Styling the heading to be responsive
+          >
+            Contact Us!
+          </h2>
+          {/* Render the ContactForm component */}
+          <ContactForm />
+        </div>
+      </section>
+    </MaxWidthWrapper>
   );
 };
 

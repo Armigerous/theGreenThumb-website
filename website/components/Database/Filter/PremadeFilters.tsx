6bec2767930b1eb9c0e4796ce90870c8f7aded48
tekahvec <tekahvec@ncsu.edu>
2025-02-18 09:34:15 -0500

    refactor: Restructure premade filters with collapsible sidebar menu

--- website/components/Database/Filter/PremadeFilters.tsx
+++ website/components/Database/Filter/PremadeFilters.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
 import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import {
   Tooltip,
@@ -12,6 +18,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { premadeFilters } from "@/types/premadeFilters";
+import { ChevronRight, Filter } from "lucide-react";
 
 interface PremadeFiltersProps {
   onApplyPremadeFilter: (filters: string[]) => void;
@@ -20,31 +27,44 @@ interface PremadeFiltersProps {
 export function PremadeFilters({ onApplyPremadeFilter }: PremadeFiltersProps) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Quick Filters</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
-          <div className="flex flex-col gap-2 p-2">
-            {premadeFilters.map((filter) => (
-              <TooltipProvider key={filter.id}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button
-                      variant="outline"
-                      size="sm"
-                      className="w-full justify-start gap-2"
-                      onClick={() => onApplyPremadeFilter(filter.filters)}
-                    >
-                      <filter.icon className="size-4" />
-                      <span className="truncate">{filter.name}</span>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{filter.description}</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ))}
-          </div>
+          <Collapsible defaultOpen asChild className="group/premadeCollapsible">
+            <SidebarMenuSubItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuSubButton>
+                  <Filter className="size-4 mr-2" />
+                  <span>Premade Filters</span>
+                  <ChevronRight className="ml-auto transition-transform group-data-[state=open]/premadeCollapsible:rotate-90" />
+                </SidebarMenuSubButton>
+              </CollapsibleTrigger>
+
+              <CollapsibleContent>
+                <div className="flex flex-col gap-2 p-2">
+                  {premadeFilters.map((filter) => (
+                    <TooltipProvider key={filter.id}>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            className="w-full justify-start gap-2"
+                            onClick={() => onApplyPremadeFilter(filter.filters)}
+                          >
+                            <filter.icon className="size-4" />
+                            <span className="truncate">{filter.name}</span>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{filter.description}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  ))}
+                </div>
+              </CollapsibleContent>
+            </SidebarMenuSubItem>
+          </Collapsible>
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
